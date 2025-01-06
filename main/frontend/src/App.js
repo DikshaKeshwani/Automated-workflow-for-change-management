31b5c282e391b51e6cb17df0e5a06d3ddab43af6
DikshaKeshwani <78577276+DikshaKeshwani@users.noreply.github.com>
2025-01-06 11:57:14 +0530

    Create App.js

---
+++ main/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ChangeForm from "./components/ChangeForm";
+import ChangeList from "./components/ChangeList";
+
+function App() {
+return (
+<div>
+<h1>Change Management Automation</h1>
+<ChangeForm />
+<ChangeList />
+</div>
+);
+}
+
+export default App;
